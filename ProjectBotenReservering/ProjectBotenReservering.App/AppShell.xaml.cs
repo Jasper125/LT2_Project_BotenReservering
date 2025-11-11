f96803c21c831686397808c57fed38881a64e045
Joppe2003 <564285@edu.rocmn.nl>
2025-11-11 10:10:25 +0100

    Setup solution

---
+++ ProjectBotenReservering/ProjectBotenReservering.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectBotenReservering.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ProjectBotenReservering/ProjectBotenReservering.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ProjectBotenReservering.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
